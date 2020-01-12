ae55d919d7a4f27f5d51d44b3409b8ea98f020da
Marco-Congedo <marco.congedo@gmail.com>
2020-01-12 20:34:17 +0100

    updated logLike iterative algorithm

--- src/optim/LogLike.jl
+++ src/optim/LogLike.jl
@@ -1,7 +1,64 @@
+#   Unit "LogLike.jl" of the Diagonalization.jl package for Julia language
+#
+#   MIT License
+#   Copyright (c) 2019, 2020
+#   Marco Congedo, CNRS, Grenoble, France:
+#   https://sites.google.com/site/marcocongedo/home
+
+# ? CONTENTS :
+#   This unit implements Dinh-Tuan Pham's algorithm based on the log-likelyhood
+#   (Kullback-Leibler divergence) criterion.
+#   D.-T. Pham (2000) Joint approximate diagonalization of positive definite
+#   matrices, SIAM Journal on Matrix Analysis and Applications, 22(4), 1136–1152.
+#
+#  This algorithm handles the AJD diagonalization procedure, corresponding
+#  to the case m=1, k>1 according to the taxonomy adopted in this package.
+#
+#  The algorithm takes as input a vector of k-real positive definite matrices
+#  𝐂 and find a non-singular matrix B such that the congruences B'*𝐂_κ*B
+#  are as diagonal as possible for all κ=1:k
+#
+#  `w` is an optional vector of k non-negative weights for each matrix in 𝐂.
+#  Pham's criterion being invariant by scaling, the weights act on the cost
+#  function, not as a weights for the entris of the matrices.
+#  Notice that the weights can be zero, amounting to ignoring the
+#  corresponding matrices. By default all weights are equal to one.
+#  A function can be passed as the `w` argument, in which case the kth weight
+#  is found as the output of this function applied to the kth matrix in 𝐂.
+#  A good choice in general is the `nonD` function declared in tools.jl unit.
+#
+#  if `whitening` = true is passed, the Jeffrey mean of the matrices in 𝐂 is
+#  computed (using the PosDefManifold.jl package) and the matrices in 𝐂
+#  are pre-transformed using the whitening matrix of the mean.
+#  Dimensionality reduction can be obtained at this stage using optional
+#  arguments `eVar` and `eVarMeth` (see documentation of the AJD constructors).
+#
+#  if sort=true (default) the column vectors of the B matrix are reordered
+#  so as to sort in descending order the diagonal elements of B'*mean(𝐂)*B,
+#  where mean(𝐂) is the arithmetic mean of the matrices in 𝐂.
+#
+#  if  `whitening` = false (default), a matrix can be provided with the `init`
+#  argument in order to initialize B.
+#
+#  `tol` is the convergence to be attained.
+#
+#  `maxiter` is the maximum number of iterations allowed.
+#
+#  if `verbose`=true, the convergence attained at each iteration and other
+#  information will be printed.
+#
+#  return: B, its pseudo-inverse, the diagonal elements of B'*mean(𝐂)*B,
+#          the number of iterations and the convergence attained
+#
+#  NB: Pham's algorithm proceeds by transforming pairs of vectors of B.
+#  A sweep goes over all (n*(n+1))/2 ij pairs, i>j. Thus it can be optimized
+#  by multi-threading the optimization of the pairs as it is done
+#  for algorithms based on Givens rotations.
+# """
 
 
 function logLike(𝐂::Union{Vector{Hermitian}, Vector{Symmetric}};
-					  w 		:: Union{Tw, Function} = ○,
+			      w 		:: Union{Tw, Function} = ○,
 				  preWhite  :: Bool = false,
 				  sort      :: Bool = true,
 				  init 	 :: Union{Symmetric, Hermitian, Nothing} = ○,
@@ -46,7 +103,8 @@ function logLike(𝐂::Union{Vector{Hermitian}, Vector{Symmetric}};
 							tmp = 𝐜[κ][ii+1]
 							𝐜[κ][ii+1] = 𝐜[κ][ii+1] + B₁₂*𝐜[κ][jj+1]
 							𝐜[κ][jj+1] = 𝐜[κ][jj+1] + B₂₁*tmp # at exit ii = ij = i + jc
-							ii, jj = ii+n, jj+n
+							ii += n
+							jj += n
 				      end
 						tmp = 𝐜[κ][i+ic+1]
 						𝐜[κ][i+ic+1] += (B₁₂*(2*𝐜[κ][ij+1] + B₁₂*𝐜[κ][jj+1]))
@@ -57,15 +115,18 @@ function logLike(𝐂::Union{Vector{Hermitian}, Vector{Symmetric}};
 							tmp = 𝐜[κ][ii+1]
 							𝐜[κ][ii+1] += B₁₂*𝐜[κ][jj+1]
 							𝐜[κ][jj+1] += B₂₁*tmp
-							ii, jj = ii+n, jj+1
-				      end
+							ii += n
+							jj += 1
+					  end
 
-				      jj, ii = jj+1, ii+1
+				      jj += 1
+					  ii += 1
 				      @inbounds while jj<(jc+n)
 							tmp = 𝐜[κ][ii+1]
 							𝐜[κ][ii+1] += B₁₂*𝐜[κ][jj+1]
 							𝐜[κ][jj+1] += B₂₁*tmp
-							ii, jj = ii+1, jj+1
+							jj += 1
+							ii += 1
 				      end
 					end
 				end
@@ -83,7 +144,7 @@ function logLike(𝐂::Union{Vector{Hermitian}, Vector{Symmetric}};
 			ic += n
 	   end # while i
 
-		#= # useless as we only need decr
+		#= useless computing the logdet crit as we only need its decrement
 		ld += (2*∑w*log(det));
 		tmp = 0.
 		for κ=1:k # OK
@@ -114,17 +175,17 @@ function logLike(𝐂::Union{Vector{Hermitian}, Vector{Symmetric}};
 	end
 	∑w=sum(w)
 
-	# pre-whitening and write matrices in vectorized form
+	# pre-whiten, initialize and write matrices in vectorized form
 	if preWhite
 		W=whitening(PosDefManifold.mean(Jeffrey, 𝐂); eVar=eVar, eVarMeth=eVarMeth)
 		𝐜=[(W.F'*C*W.F)[:] for C∈𝐂]
 		n=size(W.F, 2) # subspace dimension
 	else
-		𝐜=[C[:] for C∈𝐂]
+		# initialization only if preWhite is false
+		init≠nothing ? 𝐜=[(init'*C*init)[:] for C∈𝐂] : 𝐜=[C[:] for C∈𝐂]
 	end
 
-	# initialization only if preWhite is false
-	init≠nothing && !preWhite ? B=copy(Matrix(init')) : B=Matrix{type}(I, n, n)
+	B=Matrix{type}(I, n, n)
 
 	verbose && @info("Iterating LogLike algorithm...")
 	while true
@@ -134,16 +195,16 @@ function logLike(𝐂::Union{Vector{Hermitian}, Vector{Symmetric}};
 		(converged = conv <= tolerance) || overRun==true ? break : nothing
 		iter += 1
 	end
-	verbose ? (converged ? @info("Convergence has been attained.\n") : @warn("Convergence has not been attained.")) : nothing
+	verbose && @info("Convergence has "*converged ? "" : "not "*"been attained.\n")
 	verbose && println("")
 
 	B=Matrix(B') # get B such B'*C[k]*B is diagonal
 
 	# sort vectors of solver
 	M=mean(𝐂)
-	D=Diagonal([PosDefManifold.qf(B[:, i], M) for i=1:n])
+	D=Diagonal([PosDefManifold.quadraticForm(B[:, i], M) for i=1:n])
 	λ = sort ? _permute!(B, D, n) : diag(D)
 
 	return preWhite ? (W.F*B, pinv(B)*W.iF, λ, iter, conv) :
-							(B, pinv(B), λ, iter, conv)
+					  (B, pinv(B), λ, iter, conv)
 end
